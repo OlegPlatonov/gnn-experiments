c1e40440a4dfcac2a048990ee5d9c3e7cabb60cb
OlegPlatonov <o.a.platonov@gmail.com>
2021-11-05 19:37:58 +0300

    add cora, citeseer, pubmed datasets

--- train.py
+++ train.py
@@ -18,9 +18,9 @@ def get_args():
                         choices=['ogbn-arxiv', 'ogbn-products', 'ogbn-papers100M', 'ogbn-proteins', 'ogbn-mag',
                                  'squirrel', 'chameleon', 'actor', 'deezer-europe', 'lastfm-asia', 'facebook', 'github',
                                  'twitch-de', 'twitch-en', 'twitch-es', 'twitch-fr', 'twitch-pt', 'twitch-ru',
-                                 'flickr', 'yelp', 'fraud-yelp-chi', 'fraud-amazon', 'airports-usa', 'airports-europe',
-                                 'airports-brazil', 'deezer-hr', 'deezer-hu', 'deezer-ro', 'blogcatalog', 'ppi',
-                                 'wikipedia'])
+                                 'flickr', 'yelp', 'cora', 'citeseer', 'pubmed', 'fraud-yelp-chi', 'fraud-amazon',
+                                 'airports-usa', 'airports-europe', 'airports-brazil', 'deezer-hr', 'deezer-hu',
+                                 'deezer-ro', 'blogcatalog', 'ppi', 'wikipedia'])
     parser.add_argument('--model', type=str, default='GT', choices=['ResNet', 'GCN', 'SAGE', 'GAT', 'GT'])
     parser.add_argument('--num_layers', type=int, default=5)
     parser.add_argument('--hidden_dim', type=int, default=512)
